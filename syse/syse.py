9a3d5fe0b9ec38e6c0da6f0a29c86cdb8949bed1
apexpromgt <72336749+apexpromgt@users.noreply.github.com>
2023-04-08 12:19:02 -0700

    Update syse.py

--- syse/syse.py
+++ syse/syse.py
@@ -1,9 +1,9 @@
-"""Some simple financial calculations
-patterned after spreadsheet computations.
-There is some complexity in each function
-so that the functions behave like ufuncs with
+"""Systems & Industrial Engineering functions
+
+functions behave like ufuncs with
 broadcasting and being able to be called with scalars
 or arrays (or other sequences).
+
 Functions support the :class:`decimal.Decimal` type unless
 otherwise stated.
 """
